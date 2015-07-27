5abe2cb29090a87e85dc78e4130b8c3d43abbeed
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-07-27 16:19:13 -0400

    Break out tag info and refactor creation

--- route.go
+++ route.go
@@ -1,75 +1,9 @@
 package ripple
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
-	"strings"
 )
-
-type tagInfo []string
-
-func (t tagInfo) Method() string {
-	return t[1]
-}
-
-func (t tagInfo) Path() string {
-	return t[2]
-}
-
-func (t tagInfo) Action() string {
-	return t[0]
-}
-
-func (t tagInfo) Valid() bool {
-	return len(t) == 3
-}
-
-var (
-	errParseTagEmptyString = errors.New(
-		"parseRippleTag: cannot parse empty string")
-
-	errParseTagInvalidSplit = errors.New("parseRippleTag: invalid split length")
-)
-
-func parseActionName(tagStr string, fieldName string) (string, string) {
-	s := strings.Split(tagStr, ",")
-	name := s[0]
-	if name == "" {
-		name = fieldName
-	}
-
-	return name, s[1]
-}
-
-func parseMethPath(str string) (string, string) {
-	s := strings.Split(str, " ")
-	return s[0], s[1]
-}
-
-func parseRippleTag(field reflect.StructField) (tagInfo, error) {
-	tag := field.Tag.Get(fieldTagKey)
-	if tag == "" {
-		return nil, nil
-	}
-
-	var info tagInfo
-
-	name, methPath := parseActionName(tag, field.Name)
-	info = append(info, name)
-	meth, path := parseMethPath(methPath)
-	info = append(info, meth, path)
-
-	if !info.Valid() {
-		return nil, errParseTagInvalidSplit
-	}
-
-	return info, nil
-}
-
-func trimSlashR(path string) string {
-	return strings.TrimRight(path, "/")
-}
 
 type route struct {
 	Method  string
@@ -96,7 +30,7 @@ func getHandler(name string, v reflect.Value) (reflect.Value, error) {
 }
 
 func newRoute(v reflect.Value, field reflect.StructField) (*route, error) {
-	tInfo, err := parseRippleTag(field)
+	tInfo, err := newTagInfo(field)
 	if err != nil {
 		return nil, err
 	}
@@ -113,7 +47,7 @@ func newRoute(v reflect.Value, field reflect.StructField) (*route, error) {
 
 	return &route{
 		Method:  tInfo.Method(),
-		Path:    trimSlashR(tInfo.Path()),
+		Path:    tInfo.Path(),
 		Handler: handler,
 	}, nil
 }
